16595278f2838aa4bd745138388c8d4eeaabde10
Sara Cortes <sara.hurtado.corte@hotmail.com>
2026-08-14 16:13:41 -0300

    :tada: Adicionando Alteraçoes

--- src/pages/contador/Contador.tsx
+++ src/pages/contador/Contador.tsx
@@ -1,18 +1,25 @@
 ﻿import { useState } from "react";
-//Conectando o Hooks (ganchos) ao meu código
+import Tarefa from "../tarefa/Tarefa";
+
 function Contador() {
     const [valor, setValor] = useState<number>(0);
 
-    function handleClick(){
-      setValor(valor + 1);
+    function handleClick() {
+        setValor(valor + 1);
     }
-  return (
-    <div>
-      <h1>Componente Contador</h1>
-      <p>O valor atual é: {valor}</p>
-      <button onClick={handleClick}>Adicionar 1</button>
-    </div>
-  )
+
+    return (
+        <div>
+            <h1>Componente Contador</h1>
+            {valor === 0 ? (
+                <p>Clique para inicializar</p>
+            ) : (
+                <p>O valor atual é: {valor}</p>
+            )}
+            <button onClick={handleClick}>Adicionar 1</button>
+        </div>
+
+    );
 }
 
-export default Contador
+export default Contador;
