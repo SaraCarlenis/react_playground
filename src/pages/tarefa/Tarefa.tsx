e20683ad911d735d0ff36d7b795a440194bc710d
Sara Cortes <sara.hurtado.corte@hotmail.com>
2026-08-14 16:16:15 -0300

    :tada: Adicionando Alterações

--- src/pages/tarefa/Tarefa.tsx
+++ src/pages/tarefa/Tarefa.tsx
@@ -1,24 +1,26 @@
 ﻿import { useEffect, useState } from "react";
 
+
 function Tarefa() {
+    const [completed, setCompleted] = useState<boolean>(false)
+    const [tarefa, setTarefa] = useState<string>(" ")
 
-  const [completed, setCompleted] = useState<boolean>(false);
-  const[tarefa, setTarefa] = useState<string>('');
+    useEffect(() => {
+        if (completed) {
+            setTarefa("Parabens! Você concluiu a tarefa!");
+        }else{
+            setTarefa('');
+        }
+    }, [completed]);
 
-  useEffect(() =>{
-    if (completed) {
-      setTarefa('Parabens! Você concluiu a tarefa!');
-    }
-  }, [completed]);
-
-  return (
-    <div>
-      <h1>Componente Tarefa</h1>
-      <h3>{tarefa}</h3>
-      <p>Conclua a tarefa</p>
-      <button onClick={() => setCompleted(true)}>Concluir Tarefa</button>
-    </div>
-  )
+    return (
+        <div>
+            <h1>Componente Tarefa</h1>
+            <h3>{tarefa}</h3>
+            <p>conclua a tarefa</p>
+            <button onClick={() => completed ? setCompleted(false) : setCompleted(true)}>Concluir tarefa</button>
+        </div>
+    );
 }
 
-export default Tarefa
+export default Tarefa;
